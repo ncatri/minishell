61133d1b0d60bb6b4853ba45b84502f7dbfe9aaa
Lfourmau <loicfourmaux@gmail.com>
2021-09-27 13:47:02 +0200

    same push as the previous, just deleted comments in the top of main

--- srcs/main.c
+++ srcs/main.c
@@ -1,80 +1,9 @@
 #include "../includes/execution.h"
 
-//void first_child(int *ab)
-//{
-//	pid_t a;
-//	a = fork();
-//	if (a == 0)
-//	{
-//		close(ab[READ]);
-//		dup2(ab[WRITE], STDOUT_FILENO);
-//		//write from a to b
-//		printf("Hello world");
-//		close(ab[WRITE]);
-//		exit(1);
-//	}
-//}
-
-//void second_child(int *ab, int *bc)
-//{
-//	pid_t b;
-//	char *argv[] = {"ls", NULL};
-//	b = fork();
-//	if (b == 0)
-//	{
-//		close(bc[READ]);
-//		close(ab[WRITE]);
-//		//read from a
-//		dup2(ab[READ], STDIN_FILENO);
-//		//write from b to c
-//		dup2(bc[WRITE], STDOUT_FILENO);
-//		execve("/bin/ls", argv, NULL);
-//		exit(1);
-//	}
-//}
-
-//void third_child(int *bc)
-//{
-//	pid_t c;
-//	char *argv[] = {"wc", "-l", NULL};
-
-//	c = fork();
-//	if (c == 0)
-//	{
-//		close(bc[WRITE]);
-//		//read from b
-//		dup2(bc[READ], STDIN_FILENO);
-//		execve("/usr/bin/wc", argv, NULL);
-//		exit(1);
-//	}
-
-//}
-
-//void	create_all_childs(int **pipesfd, int nb_childs)
-//{
-//	int i;
-//	pid_t pid;
-
-//	i = -1;
-//	while (++i < nb_childs)
-//	{
-//		pid = fork();
-//		if (pid == 0)
-//		{
-//			//child
-//		}
-//		else
-//		{
-//			//dad
-//		}
-//	}
-//}
-
 int main(int argc, char **argv)
 {
-	//variables
 	(void)argc, (void)argv;
-	int	nb_cmds = 7;
+	//commands and args
 	char *cmds[] = {"/bin/ls", "/usr/bin/grep", "/bin/cat", "/usr/bin/rev", "/bin/cat", "/usr/bin/grep", "/usr/bin/wc", NULL};
 	char *first_args[] = {"ls", NULL};
 	char *scnd_args[] = {"grep", "e", NULL};
@@ -92,6 +21,8 @@ int main(int argc, char **argv)
 	args[5] = sixth_args;
 	args[6] = seventh_args;
 	args[7] = NULL;
+	//numeric datas and pipes
+	int	nb_cmds = 7;
 	int pipes_idmax = 5;
 	int pipesfd[pipes_idmax + 1][2];
 	int pipe_count;
