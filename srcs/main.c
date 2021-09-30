c02a8334b5d87cdd0681c9c9b1b7bb932d21b166
Lfourmau <loicfourmaux@gmail.com>
2021-09-30 14:46:28 +0200

    create fct to close all files. If conditions modified to economize lines. Seems to work like before <3

--- srcs/main.c
+++ srcs/main.c
@@ -1,5 +1,16 @@
 #include "../includes/execution.h"
 
+void	close_all_pipes(int pipesfd[][2], int pipesid_max)
+{
+	int j = -1;
+
+	while (++j <= pipesid_max)
+	{
+		close(pipesfd[j][READ]);
+		close(pipesfd[j][WRITE]);
+	}
+}
+
 void	fill_commands(t_command *commands, char **cmds, char *args[10][10], char *files[][20])
 {
 	int i;
@@ -11,10 +22,21 @@ void	fill_commands(t_command *commands, char **cmds, char *args[10][10], char *f
 		commands[i].args = args[i];
 		i++;
 	}
-	commands[0].input = files[0];
-	commands[3].output = files[1];
-	commands[5].input = files[2];
-	commands[6].output = files[3];
+	commands[0].input = NULL;
+	commands[0].output = NULL;
+	commands[1].input = files[0];
+	commands[1].output = NULL;
+	commands[2].input = NULL;
+	commands[2].output = files[1];
+	commands[3].input = NULL;
+	commands[3].output = NULL;
+	commands[4].input = files[2];
+	commands[4].output = NULL;
+	commands[5].input = NULL;
+	commands[5].output = files[3];
+	commands[6].input = NULL;
+	commands[6].output = NULL;
+	
 }
 
 int main(int argc, char **argv)
@@ -27,8 +49,8 @@ int main(int argc, char **argv)
 	int pipe_count;
 	int j;
 	//struct cmd
-	char *cmds[] = {"/bin/cat", "/usr/bin/grep", "/bin/cat", "/usr/bin/rev", "/bin/cat", "/usr/bin/grep", "/usr/bin/wc", NULL};
-	char *args[][10] = {{"cat", NULL}, {"grep", "file", NULL}, {"cat", NULL}, {"rev", NULL}, {"cat", "-e", NULL}, {"grep", "i", NULL}, {"wc", "-c", NULL}, {NULL}};
+	char *cmds[] = {"/bin/ls", "/usr/bin/grep", "/bin/cat", "/usr/bin/rev", "/bin/cat", "/usr/bin/grep", "/usr/bin/wc", NULL};
+	char *args[][10] = {{"ls", NULL}, {"grep", "file", NULL}, {"cat", NULL}, {"rev", NULL}, {"cat", "-e", NULL}, {"grep", "i", NULL}, {"wc", "-c", NULL}, {NULL}};
 	//char *files[]  ={"output1.txt", "input1.txt", "output2.txt", "input2.txt", "input3.txt", "output3.txt", NULL};
 	t_command commands[10];
 	char *files[][20] = {{"input1.txt", "input2.txt", "input3.txt", NULL}, {"output1.txt", NULL}, {"input4.txt", NULL}, {"output2.txt", "output3.txt", "output4.txt", NULL}, {NULL}};
@@ -45,18 +67,12 @@ int main(int argc, char **argv)
 		pipe_count = i;
 		if (fork() == 0)
 		{
-			if (i != 0)
-			{
-				//Not the first cmd -> input = previous pipe;
-				if (commands[i].input[0] == NULL)
+			//Not the first cmd and no input file -> input = previous pipe;
+			if (i != 0 && commands[i].input == NULL)
 					dup2(pipesfd[pipe_count - 1][READ] , STDIN_FILENO);
-			}
-			if (i < nb_cmds - 1)
-			{
-				//not the last cmd (i starts at 0 so if i = 6 we are at the 7th cmd) -> output = next pipe;
-				if (commands[i].output[0] == NULL)
-					dup2(pipesfd[pipe_count][WRITE], STDOUT_FILENO);
-			}
+			//not the last cmd and no output file (i starts at 0 so if i = 6 we are at the 7th cmd) -> output = next pipe;
+			if (i < nb_cmds - 1 && commands[i].output == NULL)
+				dup2(pipesfd[pipe_count][WRITE], STDOUT_FILENO);
 			//there are 1 or more inputs specifieds for the cmd
 			if (commands[i].input != NULL)
 			{
@@ -72,12 +88,7 @@ int main(int argc, char **argv)
 						dup2(open(commands[i].output[j], O_RDWR | O_TRUNC | O_CREAT, 777), 1);
 				}
 			//close
-			int j = -1;
-			while (++j <= pipes_idmax)
-			{
-				close(pipesfd[j][READ]);
-				close(pipesfd[j][WRITE]);
-			}
+			close_all_pipes(pipesfd, pipes_idmax);
 			execve(commands[i].exec, commands[i].args, NULL);
 		}
 		else
@@ -86,12 +97,7 @@ int main(int argc, char **argv)
 		}
 	}
 	//closing pipes
-	i = -1;
-	while (++i <= pipes_idmax)
-	{
-		close(pipesfd[i][READ]);
-		close(pipesfd[i][WRITE]);
-	}
+	close_all_pipes(pipesfd, pipes_idmax);
 	//waiting chidls
 	int status;
 	pid_t wait_return;
