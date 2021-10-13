35706044c3a13b582e3f1d0f9093c4fa182118bf
Lfourmau <loicfourmaux@gmail.com>
2021-10-13 08:19:27 +0200

    double chevron (appen) done

--- srcs/execution/outputs_manip.c
+++ srcs/execution/outputs_manip.c
@@ -4,12 +4,17 @@ t_error	output_redirection(t_list *output_list)
 {
 	t_list *cursor;
 	t_redir_out *output;
+	int		fd;
 
 	cursor = output_list;
 	while (cursor)
 	{
 		output = cursor->content;
-		dup2(open(output->filename, O_RDWR | O_TRUNC | O_CREAT, 0644), STDOUT_FILENO);	
+		if (output->mode == APPEND)
+			fd = open(output->filename, O_RDWR | O_APPEND | O_CREAT, 0644);
+		else
+			fd = open(output->filename, O_RDWR | O_TRUNC | O_CREAT, 0644);
+		dup2(fd, STDOUT_FILENO);	
 		cursor = cursor->next;
 	}
 	return (0);
--- srcs/main.c
+++ srcs/main.c
@@ -9,7 +9,8 @@ int main(int argc, char **argv, char **env)
 	char	*line;
 	t_list	*token_list;
 	t_command	**cmd_array;
-
+	if (argc != 1)
+		return (printf("\x1B[31mToo much args\n\033[0m"));
 	while (1)
 	{
 		line = readline("\033[0;32mminishell ===> \033[0m");
