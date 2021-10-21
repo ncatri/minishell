7ff265cad665352dafbfc800c914983132083ce0
Nicolas Catriens <nicolas.catriens@gmail.com>
2021-10-21 10:05:57 +0200

    echoctl magic fix

--- srcs/signal/signals.c
+++ srcs/signal/signals.c
@@ -7,8 +7,8 @@ void	setup_main_signals(void)
 	if (tcgetattr(STDIN_FILENO, &g_global.term_save) == -1)
 		printf("error tcgetattr\n");
 	term_buff = g_global.term_save;
-	term_buff.c_lflag |= ~ECHOCTL;
-	term_buff.c_lflag |= ECHONL;
+	term_buff.c_lflag &= ~ECHOCTL;
+//	term_buff.c_lflag |= ECHONL;
 //	term_buff.c_lflag &= ICANON;
 	tcsetattr(STDIN_FILENO, TCSANOW, &term_buff);
 
