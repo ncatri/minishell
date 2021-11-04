f8027375dc8728dbb6b7fc21858e796076cb2859
Nicolas Catriens <nicolas.catriens@gmail.com>
2021-11-04 14:56:13 +0100

    signals.c is normed

--- includes/signals.h
+++ includes/signals.h
@@ -7,10 +7,8 @@
 
 void	setup_terminal(void);
 void	setup_main_signals(void);
-void	main_signal_handler(int signal);
-void	setup_cmd_signals(void);
-void	cmd_signal_handler(int signal);
 void	handler(int signal);
 void	process(int signal);
+void	new_prompt(char c, int fd);
 
 #endif
